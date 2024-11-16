24a43303c2d1f8005b35c110eb8dae0f8c7cf5b9
zzzang12 <wadevs12@gmail.com>
2024-11-16 12:38:39 +0900

    🌈 Update(#23): Add creator icon upload UI

--- src/pages/creator/IconRegister.tsx
+++ src/pages/creator/IconRegister.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
-const RegisterIcon = (): ReactNode => {
+const IconRegister = (): ReactNode => {
   return <div>아이콘 등록</div>;
 };
 
-export default RegisterIcon;
+export default IconRegister;
