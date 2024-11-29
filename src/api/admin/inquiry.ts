97d73e36136a5a674be9a1144e99d016fc474c36
zzzang12 <wadevs12@gmail.com>
2024-11-30 06:52:47 +0900

    📍 Feat: add admin inquiΩry api

---
+++ src/api/admin/inquiry.ts
@@ -0,0 +1,59 @@
+import { apiRequest } from '..';
+import { InquiryElemResponse, InquiryResponse } from '../../types/API';
+import { MethodType } from '../../types/Enums';
+import { fetchText } from '../../utils/utils';
+
+export const getInquiryList = async (): Promise<any> => {
+  try {
+    const res = await apiRequest('/api/v3/inquiries');
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data = await res.json();
+
+    const mappedData = data.inquiryResponses.map((inquiryElem: InquiryElemResponse) => ({
+      id: inquiryElem.inquiryId,
+      title: inquiryElem.title,
+      isResolved: inquiryElem.inquiryState === 'RESOLVED',
+    }));
+    
+    return mappedData;
+  } catch (error) {
+    console.error('Failed to get inquiry list:', error);
+  }
+};
+
+export const getInquiryDetail = async (inquiryId: string): Promise<any> => {
+  try {
+    const res = await apiRequest(`/api/v3/inquiries/${inquiryId}`);
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+
+    const data: InquiryResponse = await res.json();
+
+    const mappedData = {
+      id: data.id,
+      author: data.email,
+      title: data.title,
+      content: await fetchText(data.contentsUrl),
+      createdAt: data.createdAt,
+      isResolved: data.inquiryState === 'RESOLVED',
+    };
+    
+    return mappedData;
+  } catch (error) {
+    console.error('Failed to get inquiry detail:', error);
+  }
+};
+
+export const putInquiryResolve = async (inquiryId: string): Promise<any> => {
+  try {
+    await apiRequest(`/api/v3/inquiries/${inquiryId}`, MethodType.PUT);
+  } catch (error) {
+    console.error('Failed to resolve inquiry:', error);
+  }
+};
--- src/pages/admin/InquiryList.tsx
+++ src/pages/admin/InquiryList.tsx
@@ -1,19 +1,14 @@
 import { ReactNode, useContext, useEffect, useState } from 'react';
 import { ContextProps } from '../../types/Props';
+import { Inquiry } from '../../types/Types';
 import ListElem from '../../components/ListElem';
 import Button from '../../components/Button';
 import { useNavigate } from 'react-router-dom';
 import Context from '../../contexts/Context';
 import { AdminMenu } from '../../types/Enums';
+import { getInquiryList } from '../../api/admin/inquiry';
 
-type Inquiry = {
-  id: number;
-  title: string;
-  contents: string;
-  isResolved: boolean;
-};
-
-const InquiryList = (): ReactNode => {
+const InquiryListPage = (): ReactNode => {
   const [inquiries, setInquiries] = useState<Inquiry[]>([]);
   const navigate = useNavigate();
   const { setSelectedMenu } = useContext(Context) as ContextProps;
@@ -23,33 +18,13 @@ const InquiryList = (): ReactNode => {
     navigate(`/admin/inquiries/${id}`);
   };
 
-  useEffect((): void => {
-    setInquiries([
-      {
-        id: 0,
-        title: '이미지가 안 올라갑니다',
-        contents: '제가 찍은 사진을 올렸는데...',
-        isResolved: true,
-      },
-      {
-        id: 1,
-        title: '아이콘은 어떻게 구매해요?',
-        contents: '어디로 가면 살 수 있나요?...',
-        isResolved: true,
-      },
-      {
-        id: 2,
-        title: '왤케 느리나요',
-        contents: '빠르게 해주세요...',
-        isResolved: false,
-      },
-      {
-        id: 3,
-        title: '멍 빨리 빼는 법',
-        contents: '냉찜질하면 되나여ㅛ...',
-        isResolved: false,
-      },
-    ]);
+  useEffect(() => {
+    const fetchInquiryList = async () => {
+      const data = await getInquiryList();
+      setInquiries(data);
+    };
+
+    fetchInquiryList();
   }, []);
 
   return (
@@ -58,7 +33,6 @@ const InquiryList = (): ReactNode => {
         <ListElem
           key={idx}
           title={inquiry.title}
-          subtitle={inquiry.contents}
           background={inquiry.isResolved ? 'bg-white' : 'bg-ivory'}
           divider={idx < inquiries.length - 1}
           buttons={[
@@ -71,4 +45,4 @@ const InquiryList = (): ReactNode => {
   );
 };
 
-export default InquiryList;
+export default InquiryListPage;
