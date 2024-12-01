773c8642195432b782bd0c2dea1bd2e10dd154f9
zzzang12 <wadevs12@gmail.com>
2024-12-02 00:26:49 +0900

    🔨 Fix: change api

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { ApprovalState } from '../types/Enums';
+
 export const getEnv = (key: string): string => {
   return import.meta.env[key] || 'UNDEFINED_ENV';
 };
@@ -9,4 +11,17 @@ export const isAllFieldsFilled = (fields: any[]): boolean => {
 export const fetchText = async (url: string): Promise<string> => {
   const response = await fetch(url);
   return await response.text();
+};
+
+export const toApprovalState = (state: string): ApprovalState => {
+  switch (state) {
+    case 'WAITING':
+      return ApprovalState.PENDING;
+    case 'REGISTERED':
+      return ApprovalState.APPROVED;
+    case 'REJECTED':
+      return ApprovalState.REJECTED;
+    default:
+      return ApprovalState.PENDING;
+  }
 };
