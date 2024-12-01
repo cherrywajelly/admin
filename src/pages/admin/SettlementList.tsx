f8db3bd5b5894f42ef2d7fe47e1aac495c7acd0b
zzzang12 <wadevs12@gmail.com>
2024-12-02 03:42:31 +0900

    📍 Feat: Add admin settlement api

--- src/pages/admin/SettlementList.tsx
+++ src/pages/admin/SettlementList.tsx
@@ -83,7 +83,7 @@ const SettlementListPage = (): ReactNode => {
             background={settlement.isSettled ? 'bg-white' : 'bg-ivory'}
             buttons={[
               <Button text={`${settlement.isSettled ? '정산 완료' : '정산 미완료'}`} />,
-              <Button text="상세 보기" onClick={() => handleButtonClick(settlement.id, year, month)} />,
+              <Button text="상세 보기" onClick={() => handleButtonClick(settlement.id, year + 2023, month + 1)} />,
             ]}
             divider={index < settlements.length - 1}
           />
