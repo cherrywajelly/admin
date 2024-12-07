71abaa4ca3057d68da7b2558345c1092996ace5f
chamny20 <dge3179@ajou.ac.kr>
2024-12-07 16:43:32 +0900

    📍 Feat(#96): add card component

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,24 @@
+import clsx from 'clsx';
+import { CardProps } from './Card.types';
+
+const styleVar = {
+  container:
+    'flex flex-col w-full p-6 box-border rounded-[20px] border border-gray-10 bg-white shadow-custom',
+};
+
+export const Card = (props: CardProps) => {
+  const { title, subTitle, children, className } = props;
+  return (
+    <div className={clsx(styleVar.container, className)}>
+      {title && (
+        <div className="break-keep">
+          <div className="text-body3 text-gray-80">{title}</div>
+          {/* <div className="">{subTitle}</div> */}
+          <span />
+        </div>
+      )}
+
+      <div className="">{children}</div>
+    </div>
+  );
+};
