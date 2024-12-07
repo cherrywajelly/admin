3c73c286c5cdd0651435587b04beff28613580a2
chamny20 <dge3179@ajou.ac.kr>
2024-12-08 02:07:30 +0900

    💄 Style(#96): add style keyframes and animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -139,6 +139,15 @@ const config: Config = {
       boxShadow: {
         custom: '0px 0px 8px 0px rgba(0, 0, 0, 0.07)',
       },
+      keyframes: {
+        slideUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'slide-up': 'slideUp 0.7s ease-in-out forwards',
+      },
     },
   },
   plugins: [],
